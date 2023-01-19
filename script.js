1b8cddf105d4d08a49f804d04da96fb094a7c0e8
Мария Ларичева <43138491+MariaLaricheva@users.noreply.github.com>
2023-01-19 22:07:11 +0300

    правочки

--- script.js
+++ script.js
@@ -1,110 +1,58 @@
 /*=============== HIDING HEADER ===============*/
-const buttonYellow = document.getElementById('btn-yellow')
-const header = document.getElementById('header')
+const buttonYellow = document.getElementById('btn-yellow');
+const header = document.getElementById('header');
 
-function toggleHeader() {
-    if (header.classList.contains('hidden')) {
-        header.classList.remove('hidden')
-    } else {
-        header.classList.add('hidden')
-    }
-}
-
-buttonYellow.addEventListener('click', toggleHeader);
+buttonYellow.addEventListener('click',
+    () => header.classList.toggle('hidden'));
 
 
 /*=============== ORDER SHIFT ===============*/
-const buttonGreen = document.getElementById('btn-green')
-const orderShifter = document.getElementById('order-shifter')
+const buttonGreen = document.getElementById('btn-green');
+const orderShifter = document.getElementById('order-shifter');
 
-function toggleOrder() {
-    if (orderShifter.classList.contains('order-shift')) {
-        orderShifter.classList.remove('order-shift')
-    } else {
-        orderShifter.classList.add('order-shift')
-    }
-}
-
-buttonGreen.addEventListener('click', toggleOrder)
+buttonGreen.addEventListener('click',
+    () => orderShifter.classList.toggle('order-shift'));
 
 
 /*=============== MODAL ===============*/
-const modalButton = document.getElementById('modal-btn')
-const modal = document.getElementById('modal')
+const modalButton = document.getElementById('modal-btn');
+const modal = document.getElementById('modal');
 
-
-function closeModal() {
-    modal.classList.add('hidden');
-}
-
-modalButton.addEventListener('click', closeModal);
+modalButton.addEventListener('click', () => modal.classList.add('hidden'));
 
 
 /*=============== FORM ACTION ===============*/
-const select1 = document.getElementById('datalist-1');
-const select2 = document.getElementById('datalist-2');
-const select3 = document.getElementById('datalist-3');
-const select4 = document.getElementById('datalist-4');
-const select5 = document.getElementById('datalist-5');
-const textFieldShort = document.getElementById('short-input');
-const textFieldLong = document.getElementById('long-input');
-const formButton = document.getElementById('form-button')
-
+const formButton = document.getElementById('form-button');
+const inputFields = Array.from(document.getElementsByClassName('input'));
 const formStatus = document.getElementById('form-status');
-const formContentDisplay = document.getElementById('form-content')
+const formContentDisplay = document.getElementById('form-content');
 
-const formContent = {
-    select1: undefined,
-    select2: undefined,
-    select3: undefined,
-    select4: undefined,
-    select5: undefined,
-    textFieldShort: undefined,
-    textFieldLong: undefined,
-}
+const formContent = [];
 
 const sendForm = (e) => {
     e.preventDefault();
-    // Check if the field has a value
-    if (textFieldShort.value === ''
-        || textFieldLong.value === ''
-        || select1.value === ''
-        || select2.value === ''
-        || select3.value === ''
-        || select4.value === ''
-        || select5.value === '') {
-        //show message
-        formStatus.textContent = 'Не заполнено'
+    // Check if all fields have values
+    if ( inputFields.find( el => el.value === '') ) {
+        formStatus.textContent = 'Не заполнено';
     } else {
-        formStatus.textContent = 'Успех'
+        formStatus.textContent = 'Успех';
+        //saving values
+        inputFields.forEach((field) => {
+            formContent.push(field.value);
+        })
 
-        //заполнение полей объекта
-        formContent.select1 = select1.value;
-        formContent.select2 = select2.value;
-        formContent.select3 = select3.value;
-        formContent.select4 = select4.value;
-        formContent.select5 = select5.value;
-        formContent.textFieldShort = textFieldShort.value;
-        formContent.textFieldLong = textFieldLong.value;
+        formContentDisplay.textContent = JSON.stringify(formContent);
+        localStorage.setItem('form', JSON.stringify(formContent));
 
-        formContentDisplay.textContent = JSON.stringify(formContent)
-        localStorage.setItem('form', JSON.stringify(formContent))
-
-        //remove message after 1.5 seconds
+        //removing message after 1.5 seconds
         setTimeout(() => {
-            alert('Успешно отправлено')
+            alert('Успешно отправлено');
             //clearing input fields
-            select1.value = '';
-            select2.value = '';
-            select3.value = '';
-            select4.value = '';
-            select5.value = '';
-            textFieldShort.value = '';
-            textFieldLong.value = '';
+            inputFields.map(field => {field.value = ''});
             formStatus.value = '';
             formStatus.textContent = '';
-        }, 1500)
+        }, 1500);
     }
 }
 
-formButton.addEventListener('click', sendForm)
+formButton.addEventListener('click', sendForm);
